0b7fdb55fce29b8e00d3d62aa94d0987dba82452
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-03-03 11:23:54 +0700

    lab6 edit

--- Python/lab6/103_1.py
+++ Python/lab6/103_1.py
@@ -1,5 +1,6 @@
 n = int(input())
 Dict = {}
+ans = []
 for i in range(n):
     x = input().split()
 #    print(x)
@@ -8,5 +9,11 @@ for i in range(n):
     else:
         Dict[x[1]] += (x[0], )
 #    print(Dict)
-ans = input()
-print(Dict[ans])
+fac = input().split()
+for i in fac:
+    for j in Dict[i]:
+        if not j in ans:
+            ans.append(j)
+
+for i in ans:
+    print(i, end = ' ')
