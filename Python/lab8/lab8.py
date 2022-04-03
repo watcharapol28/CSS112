dbc266a0e42259c9d0fd777c52f777820b1520e3
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-04-03 16:56:58 +0700

    sadly

--- Python/lab8/lab8.py
+++ Python/lab8/lab8.py
@@ -9,10 +9,12 @@ class boss(info):
     def __init__(self, name, age):
         super().__init__(name, age)
 
+
 class employee(info):
     salary = 20000
     def __init__(self, name, age):
         super().__init__(name, age)
+        
 
 class person(employee, boss):
     def __init__(self, name, age, status):
@@ -24,28 +26,28 @@ class person(employee, boss):
             self.salary = boss.salary
 
 
-
 class about(person):
 
     def info_(self):
         print("{} have {} years old ,Now his status is {} in company and his salary is {} ".format(self.name, self.age, self.status, self.salary))
 
-    def show_salary():
-        print("now boss have salary {}  and employee have salary {}".format(boss.salary,employee.salary))
+    def show_salary(self):
+        print("now boss have salary {}  and employee have salary {}".format(self.name, self.salary))
 
     @staticmethod
-    def salary_increase(status):
+    def salary_increase(name):
         if status == "boss":
             boss.salary += 3000
         else :
             employee.salary += 1500
     
     @staticmethod
-    def salary_decrease(status):
+    def salary_decrease(name):
         if status == "boss":
             boss.salary -= 2000
         else :
             employee.salary -= 500
+
 
 
 A1 = about("Warsss Serrr", 26, "employee")
@@ -53,5 +55,6 @@ about.salary_increase("boss")
 A1.info_()
 A2 = about("Eter Essey", 35, "boss")
 A2.info_()
-about.show_salary()
-print(A1.__class__.mro())
+A3 = about("Warssss SEE",27 ,"boss")
+A3.info_()
+#print(A1.__class__.mro())
